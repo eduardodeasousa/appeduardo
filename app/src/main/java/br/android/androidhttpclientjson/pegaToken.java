3f70e8ec554654499c3e606a1c76af241f53f6e4
Eduardo Sousa <bdrz_nofx@hotmail.com>
2016-05-09 16:13:56 -0300

    Atualizado

--- app/src/main/java/br/android/androidhttpclientjson/pegaToken.java
+++ app/src/main/java/br/android/androidhttpclientjson/pegaToken.java
@@ -39,8 +39,8 @@ public class pegaToken {
         protected String getToken() {
             JSONObject user = new JSONObject();
             try {
-                user.put("username", "eduardoadmin");
-                user.put("password", "eduardoadmin123");
+                user.put("username", "");
+                user.put("password", "");
             } catch (JSONException e) {
                 e.printStackTrace();
             }
